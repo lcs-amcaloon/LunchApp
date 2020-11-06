1a40888d7779e4d1b223a79517951bda8dc15070
Angus McAloon <AMcAloon@lcs.on.ca>
2020-11-06 10:56:59 -0500

    Initial Commit

---
+++ LunchApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LunchApp
+//
+//  Created by Angus McAloon on 2020-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LunchApp/LunchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchAppApp.swift
+//  LunchApp
+//
+//  Created by Angus McAloon on 2020-11-06.
+//
+
+import SwiftUI
+
+@main
+struct LunchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
